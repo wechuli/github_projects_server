28c9ea5a184e4ae97a33223b2770ddaadb677e38
Wechuli <wechulipaul@yahoo.com>
2020-08-24 16:10:30 +0300

    add route for oragization member change notification

--- src/routes/main.js
+++ src/routes/main.js
@@ -1,10 +1,10 @@
 const express = require("express");
-
+const genericData = require("../models/generic");
 const router = express.Router();
 
 // route for testing the server
 router.get("/", async (req, res) => {
-  const requestBody = req.body;
+  const { body } = req;
   try {
     console.log(requestBody);
     res.status(200).json({ error: false, message: "request successful" });
@@ -13,7 +13,20 @@ router.get("/", async (req, res) => {
   }
 });
 
+// memberships
 
-
+router.get("/member", async (req, res) => {
+  const { body } = req;
+  try {
+    console.log(body);
+    res
+      .status(200)
+      .json({ error: true, message: "webhook successfully received" });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: true, message: "something unexpected happened" });
+  }
+});
 
 module.exports = router;
