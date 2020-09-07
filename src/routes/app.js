f5ad2245ce369eb21ece4f467cf1915915c3784a
Wechuli <wechulipaul@yahoo.com>
2020-09-07 16:53:26 +0300

    include app auth and labels adding to opened issues

--- src/routes/app.js
+++ src/routes/app.js
@@ -1,8 +1,16 @@
 const express = require("express");
+const { App } = require("@octokit/app");
+const { request } = require("@octokit/request");
+
+const APP_ID = process.env.APP_ID; // replace with your app ID
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+const app = new App({ id: APP_ID, privateKey: PRIVATE_KEY });
+const jwt = app.getSignedJsonWebToken();
 
 const router = express.Router();
 
-router.post("/auth", (req, res) => {
+router.post("/auth", async (req, res) => {
   try {
     console.log(req.body);
     res.status(200).json({ error: false, message: "Successful" });
@@ -11,8 +19,23 @@ router.post("/auth", (req, res) => {
   }
 });
 
-router.post("/install", (req, res) => {
+router.post("/install", async (req, res) => {
+  const payload = JSON.parse(req.body["payload"]);
+  const HTTP_X_GITHUB_EVENT = req.headers["x-github-event"];
+
   try {
+    if (HTTP_X_GITHUB_EVENT === "issues" && payload["action"] == "opened") {
+      await request("POST /repos/{owner}/{repo}/issues/{issue_number}/labels", {
+        owner: payload["repository"]["owner"]["login"],
+        repo: payload["repository"]["name"],
+        issue_number: payload["issue"]["number"],
+        labels: ["needs-response"],
+        headers: {
+          authorization: `Bearer ${jwt}`,
+          accept: "application/vnd.github.machine-man-preview+json",
+        },
+      });
+    }
     console.log(req.body);
     res.status(200).json({ error: false, message: "Successful" });
   } catch (error) {
@@ -20,8 +43,12 @@ router.post("/install", (req, res) => {
   }
 });
 
-router.post("/all", (req, res) => {
+router.post("/all", async (req, res) => {
+  const payload = JSON.parse(req.body["payload"]);
+  const HTTP_X_GITHUB_EVENT = req.headers["x-github-event"];
   try {
+    if (HTTP_X_GITHUB_EVENT === "issues" && payload["action"] == "opened") {
+    }
     console.log(req.body);
     res.status(200).json({ error: false, message: "Successful" });
   } catch (error) {
