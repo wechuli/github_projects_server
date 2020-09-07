9e2bb56df2af9f8d64119ff3d23b30e88ab7929f
Wechuli <wechulipaul@yahoo.com>
2020-09-07 16:01:11 +0300

    add install endpoint

--- src/routes/app.js
+++ src/routes/app.js
@@ -11,4 +11,13 @@ router.get("/auth", (req, res) => {
   }
 });
 
+router.get("/install", (req, res) => {
+  try {
+    console.log(req.body);
+    res.status(200).json({ error: false, message: "Successful" });
+  } catch (error) {
+    res.status(500).json({ error: true, message: "Server error" });
+  }
+});
+
 module.exports = router;
