58efef13851f583f0cf4e20d5134e00778e5a79e
Wechuli <wechulipaul@yahoo.com>
2020-09-07 16:05:06 +0300

    add endpoint to catch all

--- src/routes/app.js
+++ src/routes/app.js
@@ -20,4 +20,13 @@ router.post("/install", (req, res) => {
   }
 });
 
+router.post("/all", (req, res) => {
+  try {
+    console.log(req.body);
+    res.status(200).json({ error: false, message: "Successful" });
+  } catch (error) {
+    res.status(500).json({ error: true, message: "Server error" });
+  }
+});
+
 module.exports = router;
